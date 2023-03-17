8bb6cc4c785c8c4409045824df5fee1ff1b5be35
ivan palma <jeremias.palma2022@gmail.com>
2023-03-17 12:40:04 -0300

    commit

---
+++ Fundamentos de la web/JS/eventoOnClick/script.js
@@ -0,0 +1,10 @@
+function message() {
+    // podemos incluir más código aquí si queremos
+    console.log("este mensaje proviene de script.js");
+}
+function hide(element) {
+    element.remove();
+}
+function turnOff(element) {
+    element.innerText = "logout";
+}
